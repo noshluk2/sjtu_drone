c5cb9a99c478d73b65999fba55b4117bef5fc5bd
noshluk2 <noshluk2@gmail.com>
2024-09-17 12:56:34 +0500

    drone with depth camera

--- sjtu_drone_bringup/sjtu_drone_bringup/spawn_drone.py
+++ sjtu_drone_bringup/sjtu_drone_bringup/spawn_drone.py
@@ -1,10 +1,23 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+# Copyright 2023 Georg Novotny
+#
+# Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.gnu.org/licenses/gpl-3.0.en.html
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # -*- coding: utf-8 -*-
-import os
 import sys
 import rclpy
 from gazebo_msgs.srv import SpawnEntity
-from geometry_msgs.msg import Pose
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -19,11 +32,6 @@ def main(args=None):
     req.xml = content
     req.robot_namespace = namespace
     req.reference_frame = "world"
-
-    req.initial_pose = Pose()
-    req.initial_pose.position.x = 0.0  # adjust as needed
-    req.initial_pose.position.y = 0.0  # adjust as needed
-    req.initial_pose.position.z = 1.0  # adjust as needed
 
     while not cli.wait_for_service(timeout_sec=1.0):
         node.get_logger().info('service not available, waiting again...')
--- sjtu_drone_control/setup.py
+++ sjtu_drone_control/setup.py
@@ -1,8 +1,6 @@
 from setuptools import setup
-from glob import glob
-import os
 
-package_name = 'sjtu_drone_bringup'
+package_name = 'sjtu_drone_control'
 
 setup(
     name=package_name,
@@ -12,9 +10,6 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, "launch"), glob('launch/*launch.[pxy][yma]*')),
-        (os.path.join('share', package_name, "rviz"), glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, "config"), glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -25,7 +20,10 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'spawn_drone = sjtu_drone_bringup.spawn_drone:main',
+            'teleop = sjtu_drone_control.teleop:main',
+            'teleop_joystick = sjtu_drone_control.teleop_joystick:main',
+            'open_loop_control = sjtu_drone_control.open_loop_control:main',
+            'drone_position_control = sjtu_drone_control.drone_position_control:main'
         ],
     },
 )
